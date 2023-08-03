882809c1647925773ce45b2c7150e1c5c1007b02
azharrahad31 <azherudinrahad@gmail.com>
2023-08-03 10:52:19 +0600

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,11 @@ module.exports = {
     title: `gatsby-app`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-postcss"]
+  plugins: [
+    "gatsby-plugin-postcss",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+
+],
+  
 };
